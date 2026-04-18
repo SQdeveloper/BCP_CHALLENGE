c1a3cdb9713032a4a6345250cc429f7184467994
Jefferson <jefferson.silvaquinto@gmail.com>
2026-04-18 00:30:11 -0500

    feat: configurando vite, tailwind y typescript #minor

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bcp-gradient-blue":
+          "linear-gradient(126.97deg, #0030A1 0.91%, #000A23 67.25%)",
+        "bcp-gradient-orange":
+          "linear-gradient(126.97deg, #FF7A00 0.91%, #2A0F00 67.25%)",
+      },
+    },
+  },
+  plugins: [],
+};
